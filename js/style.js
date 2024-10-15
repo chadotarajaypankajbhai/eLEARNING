3a64d35f6c0fe0e2439407c08976c7ff2c1791bd
chadotarajaypankajbhai <chadotarajay@gmail.com>
2024-10-15 10:52:20 +0530

    first commit

---
+++ js/style.js
@@ -0,0 +1,4 @@
+let about = document.getElementById("abouts");
+let courses = document.getElementById("coursess");
+let page = document.getElementById("pages");
+let contact = document.getElementById("contacts");
